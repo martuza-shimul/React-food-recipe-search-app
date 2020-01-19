4904e9bdd482bacae4cc32e8cf139db15f04498b
Martuza <martuza.shimul@gmail.com>
2020-01-20 00:51:07 +0600

    search form added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import Form from "./components/Form";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Recipe Search</h1>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">Recipe Search</h1>
+				</header>
+				<Form />
+			</div>
+		);
+	}
 }
 
 export default App;
